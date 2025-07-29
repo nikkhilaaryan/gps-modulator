65c981a2cda5154dc274741f2881b4fc6d1b9196
ARYAN RAJ <nikhilaryan0928@gmail.com>
2025-07-29 19:30:23 +0530

    refactor: improve wording and variable naming for clarity

--- gps_modulator/detectors/velocity_anomaly.py
+++ gps_modulator/detectors/velocity_anomaly.py
@@ -1,17 +1,18 @@
 from gps_modulator.utils.gps_math import compute_velocity
 
-class velocity_anomaly_detector:
+class VelocityAnomalyDetector:
     def __init__(self, threshold_velocity: float):
         self.threshold_velocity = threshold_velocity
-        self.past_point = None
+        self.previous_point = None
 
     def detect(self, present_point):
-        if self.past_point is None:
-            self.past_point = present_point
+        if self.previous_point is None:
+            self.previous_point = present_point
             return False
 
-        velocity = compute_velocity(self.past_point, present_point)
-        self.past_point = present_point
+        velocity = compute_velocity(self.previous_point, present_point)
+        self.previous_point = present_point
         if velocity > self.threshold_velocity:
             return True
         return False
+    
