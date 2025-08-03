654f7c981f56ae798ffe7d9532f329050c190195
ARYAN RAJ <nikhilaryan0928@gmail.com>
2025-08-03 23:44:59 +0530

    refactor: drop legacy detection pipeline

--- gps_modulator/core/detection_pipeline.py
+++
@@ -1,9 +0,0 @@
-from gps_modulator.detectors.velocity_anomaly import VelocityAnomalyDetector
-
-class DetectionPipeline:
-    def __init__(self, threshold_velocity: float):
-        self.velocity_anomaly_detector = VelocityAnomalyDetector(threshold_velocity)
-
-    def process_gps_point(self, present_point: dict) -> bool:
-        anomaly_detected = self.velocity_anomaly_detector.detect(present_point)
-        return anomaly_detected
